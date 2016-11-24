43a3a0efc574a8c32fba50ce601aa23d5955a76b
mzying2013 <liumin2011@gmail.com>
2016-11-24 16:56:10 +0800

    初始化项目

---
+++ TestAnchorPoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAnchorPoint
+//
+//  Created by blucy on 2016/11/24.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAnchorPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAnchorPoint
+//
+//  Created by blucy on 2016/11/24.
+//  Copyright © 2016年 bill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
